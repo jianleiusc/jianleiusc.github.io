f4e88a3504783d7febc4592371de28dd510bba45
jianlei <Jian Lei>
2015-01-11 19:48:26 -0800

    the aircraft is draggable

--- js/main.js
+++ js/main.js
@@ -10,3 +10,22 @@ function showSplash(){
 }
 
 //handle mouse drag
+$('#player').on('mousedown', function (e) {
+
+    $(this).addClass('active').parents().on('mousemove', function (e) {
+
+        $('.active').offset({
+
+            top: e.pageY - $('.active').outerHeight() / 2,
+            left: e.pageX - $('.active').outerWidth() / 2
+
+        }).on('mouseup', function () {
+
+            $(this).removeClass('active');
+
+        });
+        
+    });
+    
+    return false;    
+});
